668504a61710f99c3bd11b2871369c90f5bad828
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-06-20 21:12:45 -0700

    create homepage
    create initial homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views','src/views');
+
+
+app.get('/home', function(req,res){
+	res.render('homepage');
+});
+
+
+
+
+//Other Routes 404 not found
+app.get('*', function(req,res){
+	res.send('404 error');
+});
+
+app.listen(3000);
+
+
+
+
+/* 
 const createColony = require('./create_colony');
 const createTask = require('./create_task');
 
@@ -6,3 +30,4 @@ createColony()
   // We're exiting hard here as the providers keep polling otherwise
   .then(() => process.exit())
   .catch(err => console.error(err));
+*/
